8697e4b2d3c0fd4d6ff00df773b24ffe1b9ac6e0
Maximillian Dornseif <m.dornseif@hudora.de>
2010-01-02 23:25:25 +0100

    typofix

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 import codecs
+from setuptools import setup, find_packages
 
 setup(name='googleappsauth',
       maintainer='Maximillian Dornseif',
       maintainer_email='md@hudora.de',
       version='1.0',
       description='googleappsauth authenticates Django Users against a Google Apps Domain',
-      long_description=codecs.open('README.markdown', "r", "utf-8").read()
+      long_description=codecs.open('README.markdown', "r", "utf-8").read(),
       license='BSD',
+      url='http://github.com/hudora/django-googleappsauth',
       classifiers=['Intended Audience :: Developers',
                    'Programming Language :: Python'],
       packages = find_packages(),
@@ -16,6 +18,6 @@ setup(name='googleappsauth',
           #backend/lib/
           #backend/webapps/
       },
-      install_requires = [],
+      install_requires = ['Django'],
       zip_safe = False,
 )
